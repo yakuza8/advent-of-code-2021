e864194beb5bdbf9ee35a9ce3ea7eff4b5c0b66a
Berat Cankar <berat.cankar@udemy.com>
2022-01-21 14:34:30 +0300

    Add Day 6 part 2 - Fast solution

--- day_06.py
+++ day_06.py
@@ -15,7 +15,7 @@ class LanternFish:
     def can_give_birth(self):
         return self.next_birth <= 0
 
-    def give_birth(self, fish_list: List['LanternFish']):
+    def give_birth(self, fish_list: List["LanternFish"]):
         self.next_birth = self.BIRTH_PERIOD
         fish_list.append(LanternFish(next_birth=self.NEW_BORN_BIRTH_TIMEOUT))
 
@@ -24,6 +24,28 @@ class LanternFish:
 
 
 class Solution:
+    @staticmethod
+    def lanternfish_fast(fish: List[int], days: int):
+        from collections import defaultdict
+
+        first_birth_mapping = defaultdict(int)
+        total_fish_count = len(fish)
+
+        # Prepare initial configuration
+        for initial_fish in fish:
+            first_birth_mapping[initial_fish] += 1
+
+        # Iterate on each day
+        for day in range(1, days + 1):
+            current_fish = first_birth_mapping[day]
+
+            last_day = day + 1
+            while last_day < days + 1:
+                first_birth_mapping[last_day + 8] += current_fish
+                total_fish_count += current_fish
+                last_day += 7
+        return total_fish_count
+
     @staticmethod
     def lanternfish(fish: List[int], days: int):
         fish_list = [LanternFish(next_birth=period) for period in fish]
@@ -44,23 +66,34 @@ class Solution:
 class Tests(unittest.TestCase):
     @staticmethod
     def _read_input(fish_list):
-        return [int(_) for _ in fish_list.split(',')]
+        return [int(_) for _ in fish_list.split(",")]
 
     def test_sample_input(self):
-        fish_list = self._read_input('3,4,3,1,2')
-        self.assertEqual(26, Solution.lanternfish(fish=fish_list, days=18))
-        self.assertEqual(5934, Solution.lanternfish(fish=fish_list, days=80))
-        self.assertEqual(26984457539, Solution.lanternfish_part_two(fish=fish_list, days=256))
+        fish_list = self._read_input("3,4,3,1,2")
+
+        self.assertEqual(26, Solution.lanternfish_fast(fish=fish_list, days=18))
+        self.assertEqual(5934, Solution.lanternfish_fast(fish=fish_list, days=80))
+        self.assertEqual(
+            26984457539, Solution.lanternfish_fast(fish=fish_list, days=256)
+        )
+
+        # self.assertEqual(26, Solution.lanternfish(fish=fish_list, days=18))
+        # self.assertEqual(5934, Solution.lanternfish(fish=fish_list, days=80))
+        # self.assertEqual(26984457539, Solution.lanternfish(fish=fish_list, days=256))
 
     def test_real_problem(self):
         fish_list = self._read_input(
-            '3,5,2,5,4,3,2,2,3,5,2,3,2,2,2,2,3,5,3,5,5,2,2,3,4,2,3,5,5,3,3,5,2,4,5,4,3,5,3,2,5,4,'
-            '1,1,1,5,1,4,1,4,3,5,2,3,2,2,2,5,2,1,2,2,2,2,3,4,5,2,5,4,1,3,1,5,5,5,3,5,3,1,5,4,2,5,'
-            '3,3,5,5,5,3,2,2,1,1,3,2,1,2,2,4,3,4,1,3,4,1,2,2,4,1,3,1,4,3,3,1,2,3,1,3,4,1,1,2,5,1,'
-            '2,1,2,4,1,3,2,1,1,2,4,3,5,1,3,2,1,3,2,3,4,5,5,4,1,3,4,1,2,3,5,2,3,5,2,1,1,5,5,4,4,4,'
-            '5,3,3,2,5,4,4,1,5,1,5,5,5,2,2,1,2,4,5,1,2,1,4,5,4,2,4,3,2,5,2,2,1,4,3,5,4,2,1,1,5,1,'
-            '4,5,1,2,5,5,1,4,1,1,4,5,2,5,3,1,4,5,2,1,3,1,3,3,5,5,1,4,1,3,2,2,3,5,4,3,2,5,1,1,1,2,'
-            '2,5,3,4,2,1,3,2,5,3,2,2,3,5,2,1,4,5,4,4,5,5,3,3,5,4,5,5,4,3,5,3,5,3,1,3,2,2,1,4,4,5,'
-            '2,2,4,2,1,4')
-        print(Solution.lanternfish(fish=fish_list, days=80))
-        print(Solution.lanternfish_part_two(fish=fish_list, days=256))
+            "3,5,2,5,4,3,2,2,3,5,2,3,2,2,2,2,3,5,3,5,5,2,2,3,4,2,3,5,5,3,3,5,2,4,5,4,3,5,3,2,5,4,"
+            "1,1,1,5,1,4,1,4,3,5,2,3,2,2,2,5,2,1,2,2,2,2,3,4,5,2,5,4,1,3,1,5,5,5,3,5,3,1,5,4,2,5,"
+            "3,3,5,5,5,3,2,2,1,1,3,2,1,2,2,4,3,4,1,3,4,1,2,2,4,1,3,1,4,3,3,1,2,3,1,3,4,1,1,2,5,1,"
+            "2,1,2,4,1,3,2,1,1,2,4,3,5,1,3,2,1,3,2,3,4,5,5,4,1,3,4,1,2,3,5,2,3,5,2,1,1,5,5,4,4,4,"
+            "5,3,3,2,5,4,4,1,5,1,5,5,5,2,2,1,2,4,5,1,2,1,4,5,4,2,4,3,2,5,2,2,1,4,3,5,4,2,1,1,5,1,"
+            "4,5,1,2,5,5,1,4,1,1,4,5,2,5,3,1,4,5,2,1,3,1,3,3,5,5,1,4,1,3,2,2,3,5,4,3,2,5,1,1,1,2,"
+            "2,5,3,4,2,1,3,2,5,3,2,2,3,5,2,1,4,5,4,4,5,5,3,3,5,4,5,5,4,3,5,3,5,3,1,3,2,2,1,4,4,5,"
+            "2,2,4,2,1,4"
+        )
+        print(Solution.lanternfish_fast(fish=fish_list, days=80))
+        print(Solution.lanternfish_fast(fish=fish_list, days=256))
+
+        # print(Solution.lanternfish(fish=fish_list, days=80))
+        # print(Solution.lanternfish(fish=fish_list, days=256))
